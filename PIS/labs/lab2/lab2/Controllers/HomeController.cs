36786b65861c431e57d95ceacb0cd2bd6d72de0c
iigokingii <smolik.valera@mail.ru>
2024-02-29 19:32:36 +0300

    created lab2a on PIS

---
+++ PIS/labs/lab2/lab2/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/Index.html")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
